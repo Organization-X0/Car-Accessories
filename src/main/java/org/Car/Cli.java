3f4bd133c2f978ae10c927b44bb622025e406e33
Abod Shashtari <abod.shashtari@gmail.com>
2023-09-29 18:19:37 +0300

    Cli Colors

---
+++ src/main/java/org/Car/Cli.java
@@ -0,0 +1,17 @@
+package org.Car;
+
+import org.fusesource.jansi.Ansi;
+import static org.fusesource.jansi.Ansi.*;
+import static org.fusesource.jansi.Ansi.Color.*;
+
+public class Cli {
+    public static Ansi errorText(String text){
+        return ansi().eraseScreen().fgBright(WHITE).bgBright(RED).a(text).reset();
+    }
+    public static Ansi blueText(String text){
+        return ansi().eraseScreen().fg(BLUE).a(text).reset();
+    }
+    public static Ansi blueBoldText(String text){
+        return ansi().eraseScreen().fgBright(BLUE).a(text).reset();
+    }
+}
