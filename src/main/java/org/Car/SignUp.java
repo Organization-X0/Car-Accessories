d83c53a9fe15e4b40765b4f715c403a1aa851ecc
Abod Shashtari <abod.shashtari@gmail.com>
2023-09-29 19:02:41 +0300

    display SignUp

--- src/main/java/org/Car/SignUp.java
+++ src/main/java/org/Car/SignUp.java
@@ -3,6 +3,9 @@ package org.Car;
 import org.Data.DataBase;
 import org.Data.User;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,6 +14,20 @@ public class SignUp {
     DataBase myDataBase;
     public SignUp(DataBase dataBase){
        this.myDataBase=dataBase;
+    }
+    public Map<String,String> displaySignUp(){
+        Map<String,String> data = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(Cli.purpleBoldText("Sing Up")+"\n"+Cli.blueBoldText("Full Name: "));
+        data.put("fullname",scanner.nextLine());
+        System.out.print(Cli.blueBoldText("Email: "));
+        data.put("email",scanner.nextLine());
+        System.out.print(Cli.blueBoldText("Phone: "));
+        data.put("phone",scanner.nextLine());
+        System.out.print(Cli.blueBoldText("Password: "));
+        data.put("password",scanner.nextLine());
+        return data;
     }
     public void signUpNow(String fullName,String email,String password,String phone){
         Pattern pattern = Pattern.compile(regexPattern);
