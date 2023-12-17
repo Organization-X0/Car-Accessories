2beeba0ad9a7bb09bf43af9d40be6e5ebe52cb4e
Abod Shashtari <68024207+Abod-Shashtari@users.noreply.github.com>
2023-12-17 16:24:55 +0200

    Update RunCucumber.java

--- src/test/java/org/test/RunCucumber.java
+++ src/test/java/org/test/RunCucumber.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.car.App;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "features", glue = {"org.Test"}, plugin = {"html:target/cucumber-reports.html"})
+@CucumberOptions(features = "features", glue = {"org.test"}, plugin = {"html:target/cucumber-reports.html"})
 
 public class RunCucumber {
     App myApp;
