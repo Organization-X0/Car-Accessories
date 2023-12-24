54c4c92acb0b9fdeca71338e1be51ea7adaf76cf
Abod Shashtari <abod.shashtari@gmail.com>
2023-12-24 20:01:46 +0200

    fix-smells

--- src/test/java/org/test/SignUpSteps.java
+++ src/test/java/org/test/SignUpSteps.java
@@ -29,7 +29,7 @@ public class SignUpSteps {
 
         data.put("fullName","userx");
 
-        data.put("email","abod@gmail.com");
+        data.put("email","userx@gmail.com");
 
         data.put("phone","1234567890");
 
@@ -59,5 +59,74 @@ public class SignUpSteps {
     @Then("Show error message indicates what went wrong")
     public void show_error_message_indicates_what_went_wrong() {
        assertEquals(myApp.getError().getLocation(), myApp.getCurrentState().getStateString());
+    }
+
+    @When("full name is less than three characters and other details are valid")
+    public void fullNameIsLessThanCharactersAndOtherDetailsAreValid() {
+        data.put("fullName","us");
+
+        data.put("email","userx@gmail.com");
+
+        data.put("phone","1234567890");
+
+        data.put("password","123456aa");
+
+        myApp.getCurrentState().handleInput(data);
+
+    }
+
+    @When("full name is more than fifty characters and other details are valid")
+    public void fullNameIsMoreThanCharactersAndOtherDetailsAreValid() {
+        data.put("fullName","hahahahahahahahahahahahahahahahahahhahahahahahhahahahahahahahhahahahahahahahahhahahahahhahahahahahah");
+
+        data.put("email","userx@gmail.com");
+
+        data.put("phone","1234567890");
+
+        data.put("password","123456aa");
+
+        myApp.getCurrentState().handleInput(data);
+    }
+
+    @When("password is less than three characters and other details are valid")
+    public void passwordIsLessThanCharactersAndOtherDetailsAreValid() {
+        data.put("fullName","userx");
+
+        data.put("email","userx@gmail.com");
+
+        data.put("phone","1234567890");
+
+        data.put("password","12");
+
+        myApp.getCurrentState().handleInput(data);
+
+    }
+
+    @When("password is more than twenty characters and other details are valid")
+    public void passwordIsMoreThanCharactersAndOtherDetailsAreValid() {
+        data.put("fullName","userx");
+
+        data.put("email","userx@gmail.com");
+
+        data.put("phone","1234567890");
+
+        data.put("password","hahahahahahahahahahahahahahahahahahahahahahahahhah");
+
+        myApp.getCurrentState().handleInput(data);
+
+    }
+
+    @When("phone number is not ten digits and other details are valid")
+    public void phoneNumberIsNotDigitsAndOtherDetailsAreValid() {
+        data.put("fullName","userx");
+
+        data.put("email","userx@gmail.com");
+
+        data.put("phone","12345");
+
+        data.put("password","123456aa");
+
+        myApp.getCurrentState().handleInput(data);
+
     }
 }
